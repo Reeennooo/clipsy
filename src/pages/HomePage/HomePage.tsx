98f075ca0d93558b733b8d404a42e82e3d752c36
Reeennooo <renat.kurkin@mail.ru>
2026-06-13 13:24:24 +0300

    start

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const HomePage = () => {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main>
+        Всем привет)
+        Мы начинаем проект!
+      </main>
+    </div>
+  );
+};
